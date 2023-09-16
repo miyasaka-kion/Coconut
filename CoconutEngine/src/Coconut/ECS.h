2502d01729b67ce373f543a1989fe570d8d49fa4
Kion Miyasaka <miyasaka-kion@outlook.com>
2023-09-16 19:08:55 +0800

    ECS complete

--- CoconutEngine/src/Coconut/ECS.h
+++ CoconutEngine/src/Coconut/ECS.h
@@ -18,6 +18,8 @@ namespace Coconut {
 		return lastID++;
 	}
 	
+	// get component ID for a specific type
+	// each type has it's own, seperate type counter
 	template <typename T> 
 	inline ComponentID getComponentTypeID() noexcept {
 		static ComponentID typeID = getComponentTypeID();
@@ -41,8 +43,12 @@ namespace Coconut {
 
 	};
 
+
+
+
 	class Entity {
 	private:
+		// ? when is this initiated?
 		bool active = true;
 		std::vector<std::unique_ptr<Component> > components;
 
@@ -58,20 +64,68 @@ namespace Coconut {
 		bool isActive() const { return active;  }
 		void destroy() { active = false;  }
 
+
 		template <typename T>
 		bool hasComponent() const {
 			return ComponentBitset[getComponentID<T>()];
 		}
 
 		template <typename T, typename... TArgs>
-		T& addComponent(TArgs&& mArgs) {
+		T& addComponent(TArgs&&... mArgs) {
 			T* c(new T(std::forward<TArgs>(mArgs)...));
 			c->entity = this;
 			std::unique_ptr<Component> uPtr{ c };
 			components.emplace_back(std::move(uPtr));
 
 			componentArray[getComponentTypeID<T>()] = c;
+			componentBitset[getComponentTypeID < T < ()] = true;
+
+			c->init();
+			return *c;
+		}
+
+		template<typename T>
+		T& getComponent() const {
+			auto ptr(componentArray[getComponentTypeID<T>()]);
+			return *static_cast<T*> (ptr);
 		}
 	};
 
+	
+
+	class Manager {
+	private:
+		std::vector<std::unique_ptr<Entity> > entities;
+
+	public:
+		void update() {
+			for (auto& e : entities) e->update();
+		}
+
+		void draw() {
+			for (auto& e : entities) e->draw();
+		}
+
+		void refersh() {
+			entities.erase(
+				std::remove_if(
+					std::begin(entities),
+					std::end(entities),
+					[](const std::unique_ptr<Entity>& mEntity) {
+						return !mEntity->isActive();
+					}
+				),
+				std::end(entities)
+			);
+		}
+
+		Entity& addEntity() {
+			Entity* e = new Entity();
+			std::unique_ptr<Entity> uPtr{ e };
+			// modified version:
+			//std::unique_ptr<Entity> uPtr = std::make_unique<Entity>();
+			entities.emplace_back(std::move(uPtr));
+			return *e;
+		}
+	};
 }
