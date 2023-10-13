d7eb5284e6e9a20ce97ce9f1e424493374c4f9b2
Kion Miyasaka <miyasaka-kion@outlook.com>
2023-10-13 23:16:44 +0800

    LINK2019 bug fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <memory>
 
 #include "Application.h"
+#include <Windows.h>
 
+int CALLBACK WinMain(
+    _In_  HINSTANCE hInstance,
+    _In_  HINSTANCE hPrevInstance,
+    _In_  LPSTR lpCmdLine,
+    _In_  int nCmdShow
+);
 
 int main(int argc, char* argv[]) {
     std::unique_ptr<Application> app = std::make_unique<Application>();
